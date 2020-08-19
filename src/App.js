d0a260b20f8b06d0add3d72bba7c4941a4634f2b
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-08-19 19:02:59 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+		<header className="header">
+			<img src="https://w7.pngwing.com/pngs/241/371/png-transparent-drawing-computer-icons-lightning-angle-photography-triangle.png" alt=""/>
+		</header>
+		<nav className='nav'>
+			<div>
+				<a>Profile</a>
+			</div>
+			<div>
+				<a>Messages</a>
+			</div>
+			<div>
+				<a>News</a>
+			</div>
+			<div>
+				<a>Misic</a>
+			</div>
+			<div>
+				<a>Settings</a>
+			</div>
+		</nav>
+		<div className='content'>
+			<div>
+
+			</div>
+			<img src='https://reviewers.info/design/images/worldmap.png' alt=""/>
+			<div>
+				<img src="https://lh3.googleusercontent.com/proxy/OCKxVlaqfWDgNqw3-8vPbYVXbMWESdUjqJAtxgkcVNylQw1qP74ZPg0ACYvnapL05ouNYQdye9tgfljLsDmGQaTIdVcQSVM"/>
+			</div>
+			<div>
+				my post
+				<div>New post</div>
+				<div>
+					<div>
+						post 1
+					</div>
+					<div>
+						post 2
+					</div>
+				</div>
+			</div>
+			<div>
+
+			</div>
+		</div>
+        
     </div>
   );
 }
