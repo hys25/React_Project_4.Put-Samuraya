122b1c94ba04d13afb45e841c598814b3d92623f
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-08-29 16:19:15 +0300

    state copy in profile-reducer

--- src/index.js
+++ src/index.js
@@ -7,23 +7,14 @@ import App from './App';
 import {Provider} from 'react-redux';
 
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
-
-rerenderEntireTree(store.getState());
-
-store.subscribe( () => {
-    let state = store.getState()
-    rerenderEntireTree(state);
-});
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
