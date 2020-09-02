6cbe18fd1eeb15f0bd0028d7eb9d420fc205baaf
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-09-02 21:29:28 +0300

    API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,13 @@
 import React from 'react';
 import classes from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/userImage.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers (
-            [
-                {
-                    id: 1, 
-                    photoUrl: 'https://vokrug.tv/pic/news/4/6/c/f/46cf3fa91c478fb14807597b6b59a0a6.jpg', followed: true, 
-                    fullName : 'Roman', 
-                    status: 'I am begginer front-end developer', 
-                    location: {city: 'Kiyv', country: 'Ukraine'} 
-                },
-                {
-                    id: 2, 
-                    photoUrl: 'https://vokrug.tv/pic/news/4/6/c/f/46cf3fa91c478fb14807597b6b59a0a6.jpg', 
-                    followed: false, 
-                    fullName : 'Kate', 
-                    status: 'What are you studying?', 
-                    location: {city: 'Minsk', country: 'Belarus'} 
-                },
-                {
-                    id: 3, 
-                    photoUrl: 'https://vokrug.tv/pic/news/4/6/c/f/46cf3fa91c478fb14807597b6b59a0a6.jpg', 
-                    followed: false, 
-                    fullName : 'Karyna', 
-                    status: 'Where do you live?', 
-                    location: {city: 'New York', country: 'USA'} 
-                },
-                {
-                    id: 4, 
-                    photoUrl: 'https://vokrug.tv/pic/news/4/6/c/f/46cf3fa91c478fb14807597b6b59a0a6.jpg', 
-                    followed: true, 
-                    fullName : 'Vitalii', 
-                    status: 'I am full-stack web developer', 
-                    location: {city: 'San-Francisco', country: 'USA'} 
-                }
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
     }
 
     return (
@@ -46,7 +16,7 @@ let Users = (props) => {
             <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={classes.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto } className={classes.userPhoto} />
                     </div>
                     <div>
                         { u.followed 
@@ -58,12 +28,12 @@ let Users = (props) => {
 
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
