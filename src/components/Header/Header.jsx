ade2f7ce3d3c857c50bf7eca236a04d715badb37
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-09-09 15:46:02 +0300

    61 cookie, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return <header className={classes.header}>
         <img src="https://w7.pngwing.com/pngs/241/371/png-transparent-drawing-computer-icons-lightning-angle-photography-triangle.png" alt=""/>
+
+        <div className={classes.loginBlock}>
+            { props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink> }
+        </div>
     </header>
 }
 
