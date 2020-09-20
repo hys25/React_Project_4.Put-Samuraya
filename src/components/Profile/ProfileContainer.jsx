571fece68653df3186b882e855b82aab88661256
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-09-20 20:02:40 +0300

    79 stopSubmit (redux-form)

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import Profile from './Profile';
-import {connect} from 'react-redux';
-import {getUserProfile, getStatus, updateStatus} from '../../redux/profile-reducer';
-import {withRouter} from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
-import {withAuthRedirect} from '../../hoc/withAuthRedirect';
-import {compose} from 'redux';
-class ProfileContainer extends React.Component{
+import Profile from "./Profile";
+import * as axios from "axios";
+import {connect} from "react-redux";
+import {getStatus, getUserProfile, updateStatus} from "../../redux/profile-reducer";
+import {Redirect, withRouter} from "react-router-dom";
+import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {compose} from "redux";
+
+class ProfileContainer extends React.Component {
 
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 11414;
+            userId = this.props.authorizedUserId;
         }
         this.props.getUserProfile(userId);
-        
         this.props.getStatus(userId);
     }
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} 
-            status={this.props.status} 
-            updateStatus={this.props.updateStatus} />
+            <Profile {...this.props}
+                     profile={this.props.profile}
+                     status={this.props.status}
+                     updateStatus={this.props.updateStatus}/>
         )
     }
 }
 
-let mapStateToProps = (state) => ({ 
+let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 });
 
 export default compose(
     connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
-    withRouter,
-    //withAuthRedirect
-) (ProfileContainer);
-
+    withRouter
+)(ProfileContainer);
