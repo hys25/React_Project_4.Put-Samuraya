438019c0a12ddda6efba79f6700454f526fabb24
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-10-03 18:44:00 +0300

    90 - redux-ducks + refactoring

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
@@ -23,23 +23,23 @@ class App extends Component {
         }
 
         return (
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/>
+                    <div className='app-wrapper'>
+                        <HeaderContainer/>
+                        <Navbar/>
+                        <div className='app-wrapper-content'>
+                            <Route path='/dialogs'
+                                   render={() => <DialogsContainer/>}/>
 
-                    <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
+                            <Route path='/profile/:userId?'
+                                   render={() => <ProfileContainer/>}/>
 
-                    <Route path='/users'
-                           render={() => <UsersContainer/>}/>
+                            <Route path='/users'
+                                   render={() => <UsersContainer/>}/>
 
-                    <Route path='/login'
-                           render={() => <LoginPage/>}/>
-                </div>
-            </div>
+                            <Route path='/login'
+                                   render={() => <LoginPage/>}/>
+                        </div>
+                    </div>
         )
     }
 }
