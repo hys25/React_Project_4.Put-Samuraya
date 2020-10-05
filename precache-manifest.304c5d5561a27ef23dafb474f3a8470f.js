29e4a8990f258bbec908ff9f6f33235e7c9be14b
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-10-05 17:42:47 +0300

    Updates

---
+++ precache-manifest.304c5d5561a27ef23dafb474f3a8470f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dbabedb1e126429af2557abcee9db3",
+    "url": "/React_Project_4.Social-network/index.html"
+  },
+  {
+    "revision": "f73530358c4a2314e990",
+    "url": "/React_Project_4.Social-network/static/css/3.07819bc3.chunk.css"
+  },
+  {
+    "revision": "a48755358277993bc34d",
+    "url": "/React_Project_4.Social-network/static/css/4.982e76de.chunk.css"
+  },
+  {
+    "revision": "16e3108b9022e72cdc79",
+    "url": "/React_Project_4.Social-network/static/css/main.57e22598.chunk.css"
+  },
+  {
+    "revision": "eab01206c07dc249dda9",
+    "url": "/React_Project_4.Social-network/static/js/2.3553ae60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Project_4.Social-network/static/js/2.3553ae60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73530358c4a2314e990",
+    "url": "/React_Project_4.Social-network/static/js/3.6a8cb45f.chunk.js"
+  },
+  {
+    "revision": "a48755358277993bc34d",
+    "url": "/React_Project_4.Social-network/static/js/4.5a5a5866.chunk.js"
+  },
+  {
+    "revision": "16e3108b9022e72cdc79",
+    "url": "/React_Project_4.Social-network/static/js/main.c689ecb5.chunk.js"
+  },
+  {
+    "revision": "f3435f2ee43ce61867cf",
+    "url": "/React_Project_4.Social-network/static/js/runtime-main.cef2471a.js"
+  },
+  {
+    "revision": "588b8c26b3374ccf86a9435d84cbfd32",
+    "url": "/React_Project_4.Social-network/static/media/preloader.588b8c26.gif"
+  },
+  {
+    "revision": "b3e90dfeb6b3b2de52eddd2c94ab19ab",
+    "url": "/React_Project_4.Social-network/static/media/user.b3e90dfe.png"
+  }
+]);
